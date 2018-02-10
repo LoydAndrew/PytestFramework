565da0123ae9f807347de968652cbac71d652c1a
andrew <o.andreyev@smartweb.com.ua>
2018-02-10 20:33:33 +0200

    first iteration of framework: locators, elements, actions

---
+++ pages/home/login_page.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Locators
+    _login_link = "Login"
+    _email_field = "user_email"
+    _password_field = "user_password"
+    _login_button = "commit"
+    # End of locators
+
+    # Getting all the elements
+    def get_login(self):
+        return self.driver.find_element(By.LINK_TEXT, self._login_link)
+
+    def get_email(self):
+        return self.driver.find_element(By, self._email_field)
+
+    def get_password(self):
+        return self.driver.find_element(By, self._password_field)
+
+    def get_button(self):
+        return self.driver.find_element(By, self._login_button)
+
+    # Actions on the elements
+    def click_login(self):
+        self.get_login().click()
+
+    def enter_email(self, email):
+        self.get_password().send_keys(email)
+
+    def enter_password(self, password):
+        self.get_password().send_keys(password)
+
+    def submit_button(self):
+        self.get_button().click()
+
+    # Applying actions on the elements
+    def login(self, email, password):
+        self.click_login()
+        self.enter_email(email)
+        self.enter_password(password)
+        self.submit_button()
--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -1,45 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.home.login_page import LoginPage
+import pytest
 import unittest
 
+
 class LoginTest(unittest.TestCase):
-    @classmethod
+    @pytest.mark.run(order=1)
     def test_valid_login(self):
         driver = webdriver.Chrome()
         driver.maximize_window()
         driver.get("https://letskodeit.teachable.com/")
         driver.implicitly_wait(3)
 
-        driver.find_element(By.LINK_TEXT, "Login").click()
-        driver.find_element(By.ID, "user_email").send_keys("loyd@test.com")
-        driver.find_element(By.ID, 'user_password').send_keys("123456")
-        driver.find_element(By.NAME, 'commit').click()
+        login_page = LoginPage(driver)
+        login_page.login("loyd@test.com", "123456")
+        user_icon = driver.find_element(By.CSS_SELECTOR, '.gravatar')
 
-        userIcon = driver.find_element(By.CSS_SELECTOR, '.gravatar')
-        if userIcon is not None:
-            print("Login Successfully")
+        if user_icon is not None:
+            print("---> Login Successfully")
         else:
             print("Didn't login")
         driver.quit()
-    @classmethod
+
     def test_invalid_login(self):
         driver = webdriver.Chrome()
         driver.maximize_window()
         driver.get("https://letskodeit.teachable.com/")
         driver.implicitly_wait(3)
 
-        driver.find_element(By.LINK_TEXT, "Login").click()
-        driver.find_element(By.ID, "user_email").send_keys("loyd@testdfasd.com")
-        driver.find_element(By.ID, 'user_password').send_keys("123454123")
-        driver.find_element(By.NAME, 'commit').click()
+        login_page = LoginPage(driver)
+        login_page.login("loyd@testfa.com", "")
 
-        userIcon = driver.find_element(By.CSS_SELECTOR, '.gravatar')
-        if userIcon is not None:
-            print("Login Successfully")
-        else:
-            print("Didn't login")
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
-
-
+        error = '---> Login with invalid credentials'
+        user_icon = driver.find_element(By.CSS_SELECTOR, '.gravatar')
+        if user_icon is not None:
+            self.assertFalse(user_icon, error)  # if user_icon is not present aka TRUE  - it will pass the test
+        driver.quit()
