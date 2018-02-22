a1d18d9e4a4e57a1538683ef6c15280306bcc924
andrew <o.andreyev@smartweb.com.ua>
2018-02-22 08:27:20 +0200

    little fix

--- base/basepage.py
+++ base/basepage.py
@@ -1,22 +1,26 @@
 from base.selenium_driver import SeleniumDriver
 from traceback import print_stack
 from utilities.custom_logger import custom_logger
+from utilities.util import Util
 
 
 class BasePage(SeleniumDriver):
 
     def __init__(self, driver):
 
-        super(BasePage, self).__init__(driver):
+        super(BasePage, self).__init__(driver)
         self.driver = driver
         self.util = Util()
         self.log = custom_logger()
 
-    def verify_page_title(self,title):
+    # noinspection PyUnusedLocal
+    def verify_page_title(self, title):
 
+        # noinspection PyBroadException
         try:
             self.get_title()
-            return self.util.verify_text_contains(actual_title, title_to_verify)
+            return self.util.verify_text_contains(actual_text='',
+                                                  expected_text='')
         except:
             self.log.error("Failed to identify text")
             print_stack()
